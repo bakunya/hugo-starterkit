1dfeb18fbbbb114d69ee011d83c61fd045708f72
bakunya <irvanhakim.dev@gmail.com>
2021-12-15 11:09:49 +0700

    Remove unused files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import "./css/index.css"
-import showCategoriesModal from './js/categories_modal'
-import showSearchModal from './js/search_modal'
 
 window.addEventListener("load", () => {
-    showCategoriesModal()
-    showSearchModal()
+    /** Your script put in here */
 })
